5d7a63c76eea6cda7008fc7eaa26574283f99d58
kehilasanz <kehilasanz@gmail.com>
2026-03-22 00:03:37 -0400

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
     const response = await fetch("https://api.sola.co/v1/payments", {
       method: "POST",
       headers: {
-        "Authorization": "Bearer hachhatorebmeirbaalhaned87fba933d784b18889f4a",
+        "Authorization": "hachhatorebmeirbaalhaned87fba933d784b18889f4a",
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
